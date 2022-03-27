7f8e5e549bd9d57e326d3339fd92ebd24d14526d
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-03-28 00:29:30 +0100

    Updated Encrypter package.

--- internal/encrypter/encrypter.go
+++ internal/encrypter/encrypter.go
@@ -5,22 +5,24 @@ import (
 	"crypto/cipher"
 )
 
-type FileEncrypter struct {
-}
+type FileEncrypter struct{}
 
 func New() FileEncrypter {
 	return FileEncrypter{}
 }
 
 // Encrypt method is to encrypt any file content
+// @content attribute is the file content to be encrypted
+// @iv attribute is used by Cypher Feed Back encrypter
+// @key param should be 16, 24, or 32 bytes to create AES cypher
 // Code logic based on: https://blog.logrocket.com/learn-golang-encryption-decryption/
-func (fe *FileEncrypter) Encrypt(content []byte, iv []byte, key string) ([]byte, error) {
+func (fe *FileEncrypter) Encrypt(content []byte, iv string, key string) ([]byte, error) {
 	block, err := aes.NewCipher([]byte(key))
 	if err != nil {
 		return []byte{}, err
 	}
 
-	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb := cipher.NewCFBEncrypter(block, []byte(iv))
 	cipherText := make([]byte, len(content))
 	cfb.XORKeyStream(cipherText, content)
 	return cipherText, nil
